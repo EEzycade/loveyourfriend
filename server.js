ef2ca1a9e177c0c2956785d2f35cef0a80f107af
Cade Ellsworth <caedmd2100@gmail.com>
2021-09-21 17:20:50 -0700

    adjusted server.js to use process.env.port

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const path = require('path');
 const exphbs = require('express-handlebars');
 
 const app = express();
+const PORT = process.env.PORT || 3001;
+
 const hbs = exphbs.create({});
 
 app.engine('handlebars', hbs.engine);
